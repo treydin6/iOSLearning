91182eff545eaeb3157625369aed241c5cca17dc
treydinwinward <treydinw@gmail.com>
2020-06-30 19:44:29 -0600

    finished stories app; workiing on MVC

---
+++ Chpt-10--DestiniStoryApp/Destini-story/Destini-story/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Story.swift
+//  Destini-story
+//
+//  Created by Treydin Winward on 6/30/20.
+//  Copyright © 2020 Treydin Winward. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice1Destination: Int
+    var choice2: String
+    var choice2Destination: Int
+}
