c17d21f4af1fef001225cee5c29bbf844fa178a5
treydinwinward <treydinw@gmail.com>
2020-07-02 00:20:51 -0600

    working on coreData

---
+++ Chpt-19--TodoApp/Git-It-Done/Git-It-Done/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git-It-Done
+//
+//  Created by Treydin Winward on 7/1/20.
+//  Copyright © 2020 Treydin Winward. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
